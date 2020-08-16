a1fefc761e3600e4a4c6eda6d02d1acbc2181dd0
Quintiliano Paquini Nery <quintiliano@tecsystem.com.br>
2020-08-15 23:54:25 -0300

    describe e it

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress"/>
+
+it('Teste externo...', () =>{
+
+})
+
+describe('Grupo de teste...', ( ) =>{
+
+    describe('subgrupo de testes específicos...', () => {
+
+        it('Teste específico do subgrupo...', () =>{
+        })
+    })
+    describe('subgrupo de testes específicos 2...', () => {
+
+        it('Teste específico do subgrupo 2...', () =>{
+        })
+    })
+
+    it('Teste interno...', () =>{
+
+    })
+})
